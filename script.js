9cb2df6549856e483866d8fb3e044d27206c6ae3
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-11-15 16:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const sounds = [
+  "applause",
+  "boo",
+  "gasp",
+  "tada",
+  "victory",
+  "wrong",
+  "stop"
+];
+
+const buttons = document.querySelectorAll(".sound-btn");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const soundName = btn.textContent;
+    playSound(soundName);
+  });
+});
+
+function playSound(name) {
+  const audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
+}
